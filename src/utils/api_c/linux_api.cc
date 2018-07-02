34a351c6c9fba2023782fae5227d693480554bc3
Katarzyna Wasiuta <katarzyna.wasiuta@intel.com>
2018-07-02 13:16:28 +0200

    Linux fixes

--- src/utils/api_c/linux_api.cc
+++ src/utils/api_c/linux_api.cc
@@ -168,14 +168,14 @@ int ApiC::UnsetEnv(const std::string &name) {
   return unsetenv(name.c_str());
 }
 
-void ApiC::CountFilesWithAction(const std::string &dir,
+void ApiC::CountFilesWithAction(const std::string &path,
   std::function<void(const std::string &)> func, int* file_count) {
 
   if (auto dir = opendir(path.c_str())) {
     while (auto f = readdir(dir)) {
       if (f->d_name[0] == '.') continue;
       if (f->d_type == DT_DIR) {
-        ListFiles(path + f->d_name + "/", func, file_count);
+        CountFilesWithAction(path + f->d_name + "/", func, file_count);
       }
       if (f->d_type == DT_REG) {
         func(path + f->d_name);
